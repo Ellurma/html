d7f125b96f7905d8ea8fb1de14c763f34e08eef4
Ellurma <97447138+Ellurma@users.noreply.github.com>
2022-02-01 23:02:24 +0200

    add homework1

---
+++ JavaScript/lesson1/homework1.js
@@ -0,0 +1,104 @@
+// Створити змінні. Присвоїти кожному з них значення: 'hello','owu','com', 'ua', 1, 10, -999, 123, 3.14, 2.7, 16, true, false.
+// let a = 'hello';
+// let b = 'owu';
+// let c = 'com';
+// let d = 'ua';
+// let e = 1;
+// let f = 10;
+// let g = -999;
+// let k = 123;
+// let l = 3.14;
+// let m = 2.7;
+// let n = 16;
+// let j = 'true';
+// let p = 'false'
+// // Вивести кожну змінну за допомогою: console.log , alert, document.write
+// console.log(a);
+// console.log(b);
+// console.log(c);
+// console.log(d);
+// console.log(e);
+// console.log(f);
+// console.log(g);
+// console.log(k);
+// console.log(l);
+// console.log(m);
+// console.log(n);
+// console.log(j);
+// console.log(p);
+// document.write('<div>' + a + b + c + d + '</div>');
+// document.write(e);
+// document.write(f);
+// document.write(j);
+// document.write(m + l + g + j);
+// alert(a + b + c + d);
+// alert(e);
+// alert(k + l);
+// alert(j + p)
+// alert(e)
+// Переприсвоїти кожній змінній з завдання значення на довільне.
+// a = 'some';
+// b = 'another';
+// c = 'words';
+// d = 'here';
+// e = 5;
+// f = 15;
+// g = -555;
+// k = 987;
+// l = 4.31;
+// m = 8.6;
+// n = 27;
+// j = 'false';
+// p = 'true'
+// Вивести кожну змінну за допомогою: console.log , alert, document.write
+// console.log(a);
+// console.log(b);
+// console.log(c);
+// console.log(d);
+// console.log(e);
+// console.log(f);
+// console.log(g);
+// console.log(k);
+// console.log(l);
+// console.log(m);
+// console.log(n);
+// console.log(j);
+// console.log(p);
+// alert(a);
+// alert(b);
+// alert(c);
+// alert(d);
+// alert(e);
+// alert(f);
+// alert(g);
+// alert(k);
+// alert(l);
+// alert(m);
+// alert(n);
+// alert(j);
+// alert(p);
+// document.write(a);
+// document.write(b);
+// document.write(c);
+// document.write(d);
+// document.write(e);
+// document.write(f);
+// document.write(g);
+// document.write(k);
+// document.write(l);
+// document.write(m);
+// document.write(n);
+// document.write(j);
+// document.write(p);
+// Створити 3 змінних firstName, middleName, lastName, наповнити їх своїм ПІБ. З'єднати їх в одну змінну person. (Не об'єкт)
+let firstName = 'Olena';
+let middleName = 'Romanivna';
+let lastName = 'Shkolniak';
+console.log(firstName);
+let person=['Olena', 'Romanivna', 'Shkolniak'];
+console.log(person)
+// За допомогою 3х різних prompt() отримати 3 слова які являються вашими Імям, По-Батькові та роками.
+
+
+
+
